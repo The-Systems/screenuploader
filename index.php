9fda72ce8fce2637e241b650d4ce12941ad8f065
Moritz Walter <32977558+Nevercold@users.noreply.github.com>
2020-08-29 16:26:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,32 @@
 <?php
 include "assets/config.php";
-if(isset($_GET['auth'])){ if($_GET['auth'] == $auth) {
-    if (isset($_FILES['upload']['name'])) {
-        $extension = strtolower(pathinfo($_FILES['upload']['name'], PATHINFO_EXTENSION));
-        $filename = pathinfo($_FILES['upload']['name'], PATHINFO_FILENAME);
+if(isset($_GET['auth'])){ 
+    if($_GET['auth'] == $auth) {
+        if (isset($_FILES['upload']['name'])) {
+            $extension = strtolower(pathinfo($_FILES['upload']['name'], PATHINFO_EXTENSION));
+            $filename = pathinfo($_FILES['upload']['name'], PATHINFO_FILENAME);
 
-        if (!in_array($extension, $allowed_extensions)) {
-            die('{"success":false, "response": "bad extension - ' . $extension . '"}');
+            if (!in_array($extension, $allowed_extensions)) {
+                die('{"success":false, "response": "bad extension - ' . $extension . '"}');
+            }
+            if ($_FILES['upload']['size'] > $max_size) {
+                die('{"success":false, "response": "upload too big"}');
+            }
+
+            $key = substr(str_shuffle(str_repeat($x = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', ceil($keylength / strlen($x)))), 1, $keylength);
+            $name = $key . '.' . $extension;
+            $path = $dir ."". $name;
+            move_uploaded_file($_FILES['upload']['tmp_name'], $path);
+            die('{"success":true, "response": "' . $url . ''.$dir.'' . $name . '"}');
+
+        } else {
+
+            die('{"success":false, "response": "no upload"}');
+
         }
-        if ($_FILES['upload']['size'] > $max_size) {
-            die('{"success":false, "response": "upload too big"}');
-        }
-
-        $key = substr(str_shuffle(str_repeat($x = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', ceil($keylength / strlen($x)))), 1, $keylength);
-        $name = $key . '.' . $extension;
-        $path = $dir ."". $name;
-        move_uploaded_file($_FILES['upload']['tmp_name'], $path);
-        die('{"success":true, "response": "' . $url . ''.$dir.'' . $name . '"}');
-
     } else {
-
-        die('{"success":false, "response": "no upload"}');
-
+        die('{"success":false, "response": "wrong auth"}');
     }
-}}
+} else {
+    die('{"success":false, "response": "wrong auth"}');
+}
